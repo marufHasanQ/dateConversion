a47162d3e13626099644dac7a5c720579cfbcb3a
Maruf Hasan <maruf15-8118@diu.edu.bd>
2024-06-04 04:19:53 +0600

    new test is added

--- util.test.mjs
+++ util.test.mjs
@@ -1,62 +1,69 @@
 
-import {generateComparisonArray,getBanglaDate,getGregorianDate, checkLeapYear,getDateObjectFromInput} from './util.mjs';
+import {generateComparisonArray, getBanglaDate, getGregorianDate, checkLeapYear, getDateObjectFromInput} from './util.mjs';
 const dateString = `g2b/15/5/1977`;
 const dateObject = getDateObjectFromInput(dateString);
 const leapYearComparisonArray = generateComparisonArray(true);
 const nonLeapYearComparisonArray = generateComparisonArray(false);
-const banglaDate = getBanglaDate([nonLeapYearComparisonArray,leapYearComparisonArray])(getDateObjectFromInput('g2b/28/12/2022'));
-const gregorianDate = getGregorianDate([nonLeapYearComparisonArray,leapYearComparisonArray])(getDateObjectFromInput('b2g/13/9/1429')); 
+const banglaDate = getBanglaDate([nonLeapYearComparisonArray, leapYearComparisonArray])(getDateObjectFromInput('g2b/28/12/2022'));
+const gregorianDate = getGregorianDate([nonLeapYearComparisonArray, leapYearComparisonArray])(getDateObjectFromInput('b2g/13/9/1429'));
 
-test('checks conversion from date string to dateObject',()=>{
-    expect( Object.keys(dateObject).length).toBe(4);
+test('checks conversion from date string to dateObject', () => {
+    expect(Object.keys(dateObject).length).toBe(4);
 }
 )
 
-test('checks values of year dateObject',()=>{
+
+test('checks values of year dateObject', () => {
     expect(dateObject.year).toBe(1977);
 }
 )
 
-test('checks values of month dateObject',()=>{
+test('checks values of month dateObject', () => {
     expect(dateObject.month).toBe(5);
 }
 )
 
-test('checks values of day dateObject',()=>{
+test('checks values of day dateObject', () => {
     expect(dateObject.day).toBe(15);
 }
 )
-test('checks comparison array is adds leapyear day',()=>{
+test('checks comparison array is adds leapyear day', () => {
     expect(leapYearComparisonArray.length).toBe(366);
 }
 )
-test('checks comparison array do not add leapyear day in non leapyear',()=>{
+test('checks comparison array do not add leapyear day in non leapyear', () => {
     expect(nonLeapYearComparisonArray.length).toBe(365);
 }
 )
 
-test('checks if leapyear day is added in right position',()=>{
+test('checks if non leapyear february is 28 days', function () {
+    expect(nonLeapYearComparisonArray.at(59)[0] === 3 && nonLeapYearComparisonArray.at(59)[1] === 1).toBeTruthy();
+});
+
+test('checks if leapyear february is 29 and day is added in right position', () => {
     expect(leapYearComparisonArray.find(v => v[0] === 2 && v[1] === 29)).toBeTruthy();
 }
 )
-test('chekcs leapyear',()=>{
+test('chekcs leapyear', () => {
     expect(checkLeapYear(2004)).toBeTruthy();
 }
 )
 
-test('chekcs not leapyear',()=>{
+test('chekcs not leapyear', () => {
     expect(checkLeapYear(2007)).toBeFalsy();
 })
-test('chekcs not leapyear',()=>{
-    expect(banglaDate.year === 1429 && banglaDate.day === 13 && banglaDate.month === 9 ).toBeTruthy();
+test('chekcs not leapyear', () => {
+    expect(banglaDate.year === 1429 && banglaDate.day === 13 && banglaDate.month === 9).toBeTruthy();
 
 })
 
-test('chekcs not leapyear',()=>{
-    expect(gregorianDate.year === 2022 && gregorianDate.day === 28 && gregorianDate.month === 12 ).toBeTruthy();
+test('chekcs not leapyear', () => {
+    expect(gregorianDate.year === 2022 && gregorianDate.day === 28 && gregorianDate.month === 12).toBeTruthy();
 
 })
-console.log(getBanglaDate([nonLeapYearComparisonArray,leapYearComparisonArray])(getDateObjectFromInput('g2b/28/12/2022')));
+
+console.log(getBanglaDate([nonLeapYearComparisonArray, leapYearComparisonArray])(getDateObjectFromInput('g2b/28/12/2022')));
+
 /*
 test('checks the days of the year given dateObject',()=>{
     const dateObject = { month:2, day:5};
